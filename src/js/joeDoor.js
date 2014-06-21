30bae6b8c93f55b14bc80373c7856b93c2a9dc7e
Joe Mattelaer <josephmattelaer@gmail.com>
2014-06-21 21:35:55 +0100

    N/A

--- src/js/joeDoor.js
+++ src/js/joeDoor.js
@@ -1,9 +1,20 @@
 game.JoeDoor = me.ObjectEntity.extend({
   init: function(x, y, settings) {
+
+    console.log("joeDoor init called");
+
     this.parent(x, y, settings);
+
+    this.gravity = 0;
+
+    this.collidable = true;
   },
 
+
   onCollision: function() {
+
+    console.log("COLLISION!");
+
     game.playScreen.endLevel();
   }
 });
